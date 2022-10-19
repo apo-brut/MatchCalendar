62a12dc208a2030964c2cb8fd1e735d3285a09bc
Marcel Bohland <51537882+marcelbohland@users.noreply.github.com>
2022-10-19 12:11:33 +0200

    Basic APIs implemented

--- Backend/main.js
+++ Backend/main.js
@@ -6,8 +6,54 @@ app.get('/', (req, res) => {
   res.send('MatchCalender API Version: 1.0.0')
 })
 
-app.get('/api/test', (req, res) => {
+  // Testing API
+
+  app.get('/api/test', (req, res) => {
     res.send('Wenn du das hier zurückbekommst, dann hast du es geschaft!')
+  })
+
+  //Middleware
+
+
+  //API Calender
+
+  app.post('/api/signup', (req, res) => {
+    res.send('User angelegt')
+  })
+
+  app.post('/api/login', (req, res) => {
+    res.send('User angemeldet')
+  })
+
+  app.post('/api/calenderevent', (req, res) => {
+    res.send('Event created')
+  })
+
+  app.get('/api/calenderevent', (req, res) => {
+    res.send('Events to work with')
+  })
+
+  app.update('/api/calenderevent', (req, res) => {
+    res.send('Event updated')
+  })
+
+  app.delete('/api/calenderevent', (req, res) => {
+    res.send('Event deleted')
+  })
+
+  //API Matching
+
+  app.get('/api/userSearch', (req, res) => {
+    res.send('user123')
+  })
+
+  app.post('/api/match', (req, res) => {
+    res.send('User added')
+  })
+
+  //Settings API
+  app.update('/api/settings', (req, res) => {
+    res.send('Settings updated')
   })
 
 app.listen(port, () => {
