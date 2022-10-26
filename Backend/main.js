23dde69bfa37cfc49a8a45dd1b0fedfd1fedfe5e
Marcel Bohland <51537882+marcelbohland@users.noreply.github.com>
2022-10-26 08:19:23 +0200

    public app added

--- Backend/main.js
+++ Backend/main.js
@@ -20,6 +20,12 @@ nconf.file({
   app.use(helmet())
 
 
+  app.all('/app/*', function (req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('./public/index.html', {
+      root: __dirname
+    });
+  });
 
   /**
    * API
