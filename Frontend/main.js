51a0e87e46cc9aa28435d7740e3ba61410bf94e0
marcelbohland <51537882+marcelbohland@users.noreply.github.com>
2022-11-14 15:27:17 +0100

    Move js from index.html to main.js (CORS issue fix)

--- Frontend/main.js
+++ Frontend/main.js
@@ -125,6 +125,83 @@ var i = "";
         }
 //js für form
 
+var abfrage = window.confirm("Schön das du da bist! Um diese Anwendung benuzen zu können musst du dich regestrieren in dem du auf (OK) klickst. Wenn du bereits einen Account bei uns hast, kannst du dich ganz einfach anmelden");
+    
+if (abfrage == true){
+    document.getElementById("myModal").style.display="block";
+}
+/* Account */
+var objpeople = [];
+
+console.log(objpeople);
+function getinfo() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    objpeople = JSON.parse(localStorage.getItem("user"));
+    for(i = 0; i < objpeople.length; i++) {
+        if (username == objpeople [i].username && password == objpeople[i].password) {
+            sessionStorage.setItem("currentlooedin",username)
+            alert(username+" is logged in!!")
+            document.getElementById("myModal").style.display="none";
+            return
+        }
+    }
+    for(i = 0; i < objpeople.length; i++) {
+        if (username == objpeople [i].username && password == objpeople[i].password) {
+            alert(" falscher Username oder falsches Passwort")
+            return
+        }
+    }
+    
+}
+function registeruser(){
+    var registeruser = document.getElementById("newuser").value
+    var registerpassword = document.getElementById("newpassword").value
+    var registeremail    = document.getElementById("newemail").value
+
+    var newuser = {
+        username: registeruser,
+        email: registeremail,
+        password: registerpassword,
+
+    }
+    for(i = 0; i < objpeople.length; i++) {
+
+        if(registeruser == objpeople [i].username){
+            alert("Dieser Name ist schon vorhanden, bitte wähle einen anderen Namen")
+            return
+
+
+        }
+            if(registeremail == objpeople[i].email){
+                alert("Diese Email-Adresse ist bereits vorhanden, bitte wählen Sie einen anderen Namen")
+                return
+            }
+
+         if (registerpassword.length < 8){
+            alert("Das Passwort muss mindestens 8 ziffern haben")
+            return
+        }
+        
+
+    }
+    if(localStorage.getItem("user")==null){
+        objpeople.push(newuser);
+        localStorage.setItem("user",JSON.stringify(objpeople));
+     }
+     else
+     {
+        objpeople = JSON.parse(localStorage.getItem("user"));
+        objpeople.push(newuser);
+        localStorage.setItem("user",JSON.stringify(objpeople));
+     }
+    console.log(objpeople)
+
+    document.getElementById("newuser").value="";
+    document.getElementById("newpassword").value="";
+    document.getElementById("newemail").value="";
+}
+
      
          
 
